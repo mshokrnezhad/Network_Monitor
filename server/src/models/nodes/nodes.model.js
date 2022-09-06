8af417a369cc034a52f0cfa177bb6a8f8ef80178
Masoud <m.shokrnezhad@gmail.com>
2022-09-06 17:37:56 +0200

    the process of initializing miniclouds is updated by adding the official API of ACCORDION.

--- server/src/models/nodes/nodes.model.js
+++ server/src/models/nodes/nodes.model.js
@@ -2,33 +2,56 @@ const fs = require("fs");
 const nodes = require("./nodes.mongo");
 const { parse } = require("csv-parse");
 const path = require("path");
+const axios = require("axios");
 
-function loadNodesData() {
-  var nodeCounter = 0;
+async function loadMiniclouds() {
+  try {
+    const response = await axios({
+      method: "get",
+      url: `${process.env.MMM_API}`,
+    });
 
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(
-      path.join(__dirname, "..", "..", "..", "data", "nodes.csv")
-    )
-      .pipe(
-        parse({
-          comment: "#",
-          columns: true,
-        })
-      )
-      .on("data", async (data) => {
-        nodeCounter += 1;
-        await saveNode(data);
-      })
-      .on("error", (error) => {
-        console.log(`saving nodes is failed: ${error}`);
-        reject();
-      })
-      .on("end", async () => {
-        console.log(`the number of nodes is ${nodeCounter}.`);
-        resolve();
-      });
-  });
+    if (response.status == 200) {
+      const miniclouds = response.data;
+      const minicloudCounter = miniclouds.length;
+
+      if (minicloudCounter == 0) {
+        return 0;
+      } else
+        console.log(nodeCounter);
+        // nodeCounter += 1;
+        // await saveNode(data);
+        // return await response.data;
+    } else {
+      console.log("😩 getting the list of miniclouds is failed!");
+    }
+  } catch {
+    console.log("😩 getting the list of miniclouds is failed!");
+  }
+
+  // return new Promise((resolve, reject) => {
+  //   fs.createReadStream(
+  //     path.join(__dirname, "..", "..", "..", "data", "nodes.csv")
+  //   )
+  //     .pipe(
+  //       parse({
+  //         comment: "#",
+  //         columns: true,
+  //       })
+  //     )
+  //     .on("data", async (data) => {
+  //       nodeCounter += 1;
+  //       await saveNode(data);
+  //     })
+  //     .on("error", (error) => {
+  //       console.log(`saving nodes is failed: ${error}`);
+  //       reject();
+  //     })
+  //     .on("end", async () => {
+  //       console.log(`the number of nodes is ${nodeCounter}.`);
+  //       resolve();
+  //     });
+  // });
 }
 
 async function saveNode(data) {
@@ -62,6 +85,6 @@ async function getAllNodes() {
 }
 
 module.exports = {
-  loadNodesData,
+  loadMiniclouds,
   getAllNodes,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./app");
 require("dotenv").config();
-const { loadNodesData } = require("./models/nodes/nodes.model");
+const { loadMiniclouds } = require("./models/nodes/nodes.model");
 const { connectToMongodb } = require("../src/services/mongodb");
 const PORT = process.env.PORT || 8000;
 
@@ -9,11 +9,16 @@ const server = http.createServer(app);
 
 async function startServer() {
   await connectToMongodb();
-  await loadNodesData();
+  const minicloudCounter = await loadMiniclouds();
 
-  server.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`);
-  });
+  if (minicloudCounter == 0) {
+    console.log("😩 there is no minicloud available!");
+    return 0;
+  } else {
+    server.listen(PORT, () => {
+      console.log(`Listening on ${PORT}...`);
+    });
+  }
 }
 
 startServer();
